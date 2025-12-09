f643b6859054467f8fdc4fef13e4dd68d4506739
aima kareem <amikareem921@gmail.com>
2025-12-09 17:48:38 +0500

    create button

---
+++ components/BackBtn.tsx
@@ -0,0 +1,14 @@
+'use client';
+import Link from "next/link"; 
+export default function BackBtn() {
+    return (
+
+        <Link href="\page.tsx">
+            <button className=" bg-blue-500 text-white font-medium text-xl 
+            rounded-2xl  h-12 w-40 flex items-center justify-center
+            hover:bg-gray-700 hover:scale-105 transition-transform">
+                Back
+            </button>
+        </Link>
+    );
+}
